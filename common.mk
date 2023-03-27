2635d1b8660068895a95ebec3d884723bad8a215
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    sdm632-common: Update Wifi to use the AIDL HAL
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- common.mk
+++ common.mk
@@ -451,7 +451,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     hostapd_cli \
     libnl \
