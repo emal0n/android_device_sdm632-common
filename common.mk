d87082908290217b1f1fc2fa2955f8cc9b0b63a1
L1ghtzin <lightzin021@gmail.com>
2024-03-05 09:52:45 -0300

    sdm632-common: Use prebuilt kernel-headers

--- common.mk
+++ common.mk
@@ -287,6 +287,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Prebuilt Headers
+PRODUCT_VENDOR_KERNEL_HEADERS := kernel/motorola/kernel-headers
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
