a22ae84b9bca98c79337c6cd7b49b6c0205125f9
Vhmit <viktor.9630@protonmail.com>
2024-06-09 10:36:21 -0300

    sdm632-common: Build android.frameworks.displayservice@1.0
    * E/vndksupport(4252): Could not load /vendor/lib/hw/camera.msm8953.so from default namespace: dlopen failed: library "android.frameworks.displayservice@1.0.so" not found: needed by /vendor/lib/hw/camera.msm8953.so in namespace (default).
    
    Change-Id: I7303fc1dae10868917705c47aaa06260a6d06e95
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- common.mk
+++ common.mk
@@ -80,6 +80,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
