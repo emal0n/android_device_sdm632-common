afaaa342b297c1e2682b8080c1db1f789e1876e0
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:50:24 +0300

    sdm632-common: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,6 +116,7 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/derp/config/device_framework_matrix.xml
